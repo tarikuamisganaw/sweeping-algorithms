10c9f2a3900330ca001b5db38043cb8711790cc2
Luke Peterson <luke@infoplatter.com>
2025-01-19 18:33:52 +0900

    Adding "ordinary" (non-jumping) catamorphism

--- src/morphisms.rs
+++ src/morphisms.rs
@@ -60,7 +60,7 @@
 use crate::zipper::*;
 
 pub trait ZipperMorphisms<V> {
-    /// Applies a "jumping" catamorphism to the trie
+    /// Applies an ordinary (non-jumping) catamorphism to the trie
     ///
     /// ## Args
     /// - `map_f`: `mapper(v: &V, path: &[u8]) -> W`
@@ -73,104 +73,138 @@ pub trait ZipperMorphisms<V> {
     /// Aggregates the results from the child branches, `cs`, descending from `path` into a single result
     ///
     /// In all cases, the `path` arg is the [origin_path](ZipperAbsolutePath::origin_path)
-    fn into_jumping_cata_side_effect<W, MapF, CollapseF, AlgF>(self, map_f: MapF, collapse_f: CollapseF, alg_f: AlgF) -> W
+    fn into_cata_side_effect<W, MapF, CollapseF, AlgF>(self, map_f: MapF, collapse_f: CollapseF, alg_f: AlgF) -> W
         where
         MapF: FnMut(&V, &[u8]) -> W,
         CollapseF: FnMut(&V, W, &[u8]) -> W,
         AlgF: FnMut(&[u64; 4], &mut [W], &[u8]) -> W;
 
+    /// Applies a "jumping" catamorphism to the trie
+    ///
+    /// See [into_cata_side_effect](ZipperMorphisms::into_cata_side_effect) for a description of the args
+    fn into_jumping_cata_side_effect<W, MapF, CollapseF, AlgF>(self, map_f: MapF, collapse_f: CollapseF, alg_f: AlgF) -> W
+        where
+        MapF: FnMut(&V, &[u8]) -> W,
+        CollapseF: FnMut(&V, W, &[u8]) -> W,
+        AlgF: FnMut(&[u64; 4], &mut [W], &[u8]) -> W;
 }
 
 impl<'a, Z, V: 'a> ZipperMorphisms<V> for Z where Z: ReadOnlyZipper<'a, V> + ZipperAbsolutePath {
-    fn into_jumping_cata_side_effect<W, MapF, CollapseF, AlgF>(mut self, mut map_f: MapF, mut collapse_f: CollapseF, mut alg_f: AlgF) -> W
+    fn into_jumping_cata_side_effect<W, MapF, CollapseF, AlgF>(self, map_f: MapF, collapse_f: CollapseF, alg_f: AlgF) -> W
         where
         MapF: FnMut(&V, &[u8]) -> W,
         CollapseF: FnMut(&V, W, &[u8]) -> W,
         AlgF: FnMut(&[u64; 4], &mut [W], &[u8]) -> W
     {
-        //`stack` holds a "frame" at each forking point above the zipper position.  No frames exist for values
-        let mut stack = Vec::<StackFrame<W>>::with_capacity(12);
-        let mut frame_idx = 0;
+        cata_side_effect_body::<Self, V, W, MapF, CollapseF, AlgF, true>(self, map_f, collapse_f, alg_f)
+    }
 
-        self.reset();
+    fn into_cata_side_effect<W, MapF, CollapseF, AlgF>(self, map_f: MapF, collapse_f: CollapseF, alg_f: AlgF) -> W
+        where
+        MapF: FnMut(&V, &[u8]) -> W,
+        CollapseF: FnMut(&V, W, &[u8]) -> W,
+        AlgF: FnMut(&[u64; 4], &mut [W], &[u8]) -> W
+    {
+        cata_side_effect_body::<Self, V, W, MapF, CollapseF, AlgF, false>(self, map_f, collapse_f, alg_f)
+    }
+}
 
-        //Push a stack frame for the root, and start on the first branch off the root
-        stack.push(StackFrame::new(self.child_count(), self.child_mask()));
-        if !self.descend_first_byte() {
-            //Empty trie is a special case
-            return alg_f(&[0; 4], &mut [], self.origin_path().unwrap());
-        }
+#[inline(always)]
+fn cata_side_effect_body<'a, Z, V: 'a, W, MapF, CollapseF, AlgF, const JUMPING: bool>(mut z: Z, mut map_f: MapF, mut collapse_f: CollapseF, mut alg_f: AlgF) -> W
+    where
+    Z: ReadOnlyZipper<'a, V> + ZipperAbsolutePath,
+    MapF: FnMut(&V, &[u8]) -> W,
+    CollapseF: FnMut(&V, W, &[u8]) -> W,
+    AlgF: FnMut(&[u64; 4], &mut [W], &[u8]) -> W
+{
+    //`stack` holds a "frame" at each forking point above the zipper position.  No frames exist for values
+    let mut stack = Vec::<StackFrame<W>>::with_capacity(12);
+    let mut frame_idx = 0;
 
-        loop {
-            //Descend to the next forking point
-            let mut at_leaf = false;
-            while self.child_count() < 2 {
-                if !self.descend_until() {
-                    at_leaf = true;
+    z.reset();
+
+    //Push a stack frame for the root, and start on the first branch off the root
+    stack.push(StackFrame::new(z.child_count(), z.child_mask()));
+    if !z.descend_first_byte() {
+        //Empty trie is a special case
+        return alg_f(&[0; 4], &mut [], z.origin_path().unwrap());
+    }
+
+    loop {
+        //Descend to the next forking point if we're jumping
+        if JUMPING {
+            while z.child_count() < 2 {
+                if !z.descend_until() {
                     break;
                 }
             }
+        }
 
-            if at_leaf {
-                //Map the value from this leaf
-                let cur_w = self.get_value().map(|v| map_f(v, self.origin_path().unwrap()));
+        if z.child_count() == 0 {
+            //Map the value from this leaf
+            let mut cur_w = z.get_value().map(|v| map_f(v, z.origin_path().unwrap()));
 
+            if JUMPING {
                 //Ascend back to the last fork point
-                let cur_w = ascend_to_fork(&mut self, &mut map_f, &mut collapse_f, cur_w);
-
-                //Merge the result into the stack frame
-                match cur_w {
-                    Some(w) => stack[frame_idx].push_val(w),
-                    None => stack[frame_idx].push_none(),
-                }
-
-                debug_assert!(stack[frame_idx].child_idx <= self.child_count());
-                while stack[frame_idx].child_idx == self.child_count() {
-
-                    //We finished all the children from this branch, so run the aggregate function
-                    let stack_frame = &mut stack[frame_idx];
-                    let mut w = alg_f(&stack_frame.child_mask, &mut stack_frame.children, self.origin_path().unwrap());
-                    if frame_idx == 0 {
-                        return w
-                    } else {
-                        frame_idx -= 1;
-
-                        //Check to see if we have a value here we need to collapse
-                        if let Some(v) = self.get_value() {
-                            w = collapse_f(v, w, self.origin_path().unwrap());
-                        }
-
-                        //Ascend the rest of the way back up to the branch
-                        let w = ascend_to_fork(&mut self, &mut map_f, &mut collapse_f, Some(w)).unwrap();
-
-                        //Merge the result into the stack frame
-                        stack[frame_idx].push_val(w);
-                        debug_assert!(stack[frame_idx].child_idx <= self.child_count());
-                    }
-                }
-
-                //Position to descend the next child branch
-                let descended = self.descend_indexed_branch(stack[frame_idx].child_idx);
-                debug_assert!(descended);
-
+                cur_w = ascend_to_fork(&mut z, &mut map_f, &mut collapse_f, cur_w);
             } else {
-                //Push a new stack frame for this branch
-                frame_idx += 1;
-                let child_mask = self.child_mask();
-                let child_cnt = self.child_count();
-                if stack.len() <= frame_idx {
-                    stack.push(StackFrame::new(child_cnt, child_mask));
-                    debug_assert!(stack.len() > frame_idx);
-                } else {
-                    stack[frame_idx].reset(child_mask);
-                }
-
-                //Descend the first child branch
-                self.descend_first_byte();
+                z.ascend_byte();
             }
+
+            //Merge the result into the stack frame
+            match cur_w {
+                Some(w) => stack[frame_idx].push_val(w),
+                None => stack[frame_idx].push_none(),
+            }
+
+            debug_assert!(stack[frame_idx].child_idx <= z.child_count());
+            while stack[frame_idx].child_idx == z.child_count() {
+
+                //We finished all the children from this branch, so run the aggregate function
+                let stack_frame = &mut stack[frame_idx];
+                let mut w = alg_f(&stack_frame.child_mask, &mut stack_frame.children, z.origin_path().unwrap());
+                if frame_idx == 0 {
+                    return w
+                } else {
+                    frame_idx -= 1;
+
+                    //Check to see if we have a value here we need to collapse
+                    if let Some(v) = z.get_value() {
+                        w = collapse_f(v, w, z.origin_path().unwrap());
+                    }
+
+                    if JUMPING {
+                        //Ascend the rest of the way back up to the branch
+                        w = ascend_to_fork(&mut z, &mut map_f, &mut collapse_f, Some(w)).unwrap();
+                    } else {
+                        z.ascend_byte();
+                    }
+
+                    //Merge the result into the stack frame
+                    stack[frame_idx].push_val(w);
+                    debug_assert!(stack[frame_idx].child_idx <= z.child_count());
+                }
+            }
+
+            //Position to descend the next child branch
+            let descended = z.descend_indexed_branch(stack[frame_idx].child_idx);
+            debug_assert!(descended);
+        } else {
+            //Push a new stack frame for this branch
+            frame_idx += 1;
+            let child_mask = z.child_mask();
+            let child_cnt = z.child_count();
+            if stack.len() <= frame_idx {
+                stack.push(StackFrame::new(child_cnt, child_mask));
+                debug_assert!(stack.len() > frame_idx);
+            } else {
+                stack[frame_idx].reset(child_mask);
+            }
+
+            //Descend the first child branch
+            z.descend_first_byte();
         }
     }
-
 }
 
 #[inline(always)]
@@ -181,7 +215,6 @@ fn ascend_to_fork<'a, Z, V: 'a, W, MapF, CollapseF>(z: &mut Z, map_f: &mut MapF,
     CollapseF: FnMut(&V, W, &[u8]) -> W,
 {
     loop {
-
         z.ascend_until();
 
         if z.at_root() {
@@ -270,7 +303,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn jumping_cata_test1() {
+    fn cata_test1() {
         let tests = [
             (vec![], 0), //Empty special case
             (vec!["1"], 1), //A branch at the root
@@ -288,45 +321,72 @@ mod tests {
             let map: BytesTrieMap<()> = keys.into_iter().map(|v| (v, ())).collect();
             let zip = map.read_zipper();
 
-            let sum = zip.into_jumping_cata_side_effect(
-                |_v, path| {
-                    let val = (*path.last().unwrap() as char).to_digit(10).unwrap();
-                    // println!("map path=\"{}\" val={val}", String::from_utf8_lossy(path));
-                    val
-                },
-                |_v, upstream, path| {
-                    let this_digit = (*path.last().unwrap() as char).to_digit(10).unwrap();
-                    // println!("collapse path=\"{}\", upstream={upstream}, this_digit={this_digit}, sum={}", String::from_utf8_lossy(path), upstream + this_digit);
-                    upstream + this_digit
-                },
-                |_child_mask, children, _path| {
-                    // println!("aggregate path=\"{}\", children={children:?}", String::from_utf8_lossy(_path));
-                    children.into_iter().fold(0, |sum, child| sum + *child)
-                }
-            );
-            // println!("sum = {sum}\n");
+            let map_f = |_v: &(), path: &[u8]| {
+                let val = (*path.last().unwrap() as char).to_digit(10).unwrap();
+                // println!("map path=\"{}\" val={val}", String::from_utf8_lossy(path));
+                val
+            };
+            let collapse_f = |_v: &(), upstream: u32, path: &[u8]| {
+                let this_digit = (*path.last().unwrap() as char).to_digit(10).unwrap();
+                // println!("collapse path=\"{}\", upstream={upstream}, this_digit={this_digit}, sum={}", String::from_utf8_lossy(path), upstream + this_digit);
+                upstream + this_digit
+            };
+            let alg_f = |_child_mask: &[u64; 4], children: &mut [u32], _path: &[u8]| {
+                // println!("aggregate path=\"{}\", children={children:?}", String::from_utf8_lossy(_path));
+                children.into_iter().fold(0, |sum, child| sum + *child)
+            };
+
+            //Test both the jumping and non-jumping versions, since they ought to do the same thing
+            let sum = zip.clone().into_cata_side_effect(map_f, collapse_f, alg_f);
+            assert_eq!(sum, expected_sum);
+
+            let sum = zip.into_jumping_cata_side_effect(map_f, collapse_f, alg_f);
             assert_eq!(sum, expected_sum);
         }
     }
 
     #[test]
-    fn jumping_cata_test2() {
+    fn cata_test2() {
         let mut btm = BytesTrieMap::new();
         let rs = ["arrow", "bow", "cannon", "roman", "romane", "romanus", "romulus", "rubens", "ruber", "rubicon", "rubicundus", "rom'i"];
         rs.iter().enumerate().for_each(|(i, r)| { btm.insert(r.as_bytes(), i); });
 
+        //These algorithms should perform the same with both "jumping" and "non-jumping" versions
+
         // like val count, but without counting internal values
+        let cnt = btm.read_zipper().into_cata_side_effect(
+            |_v, _p| { 1usize },
+            |_v, w, _p| { w },
+            |_m, ws, _p| { ws.iter().sum() });
+        assert_eq!(cnt, 11);
+
         let cnt = btm.read_zipper().into_jumping_cata_side_effect(
             |_v, _p| { 1usize },
             |_v, w, _p| { w },
             |_m, ws, _p| { ws.iter().sum() });
         assert_eq!(cnt, 11);
 
+        let longest = btm.read_zipper().into_cata_side_effect(
+            |_v, p| { p.to_vec() },
+            |_v, w, _p| { w },
+            |_m, ws: &mut [Vec<u8>], _p| { ws.iter_mut().max_by_key(|p| p.len()).map_or(vec![], std::mem::take) });
+        assert_eq!(std::str::from_utf8(longest.as_slice()).unwrap(), "rubicundus");
+
         let longest = btm.read_zipper().into_jumping_cata_side_effect(
             |_v, p| { p.to_vec() },
             |_v, w, _p| { w },
             |_m, ws: &mut [Vec<u8>], _p| { ws.iter_mut().max_by_key(|p| p.len()).map_or(vec![], std::mem::take) });
         assert_eq!(std::str::from_utf8(longest.as_slice()).unwrap(), "rubicundus");
+
+        let at_truncated = btm.read_zipper().into_cata_side_effect(
+            |_v, _p| { vec![] },
+            |v, _w, _p| { vec![v.clone()] },
+            |_m, ws: &mut [_], _p| {
+                let mut r = ws.first_mut().map_or(vec![], std::mem::take);
+                for w in ws[1..].iter_mut() { r.extend(w.drain(..)); }
+                r
+            });
+        assert_eq!(at_truncated, vec![3]);
 
         let at_truncated = btm.read_zipper().into_jumping_cata_side_effect(
             |_v, _p| { vec![] },
@@ -338,4 +398,46 @@ mod tests {
             });
         assert_eq!(at_truncated, vec![3]);
     }
+
+    #[test]
+    fn cata_test3() {
+        let tests = [
+            (vec![], 0, 0),
+            (vec!["i"], 1, 1), //1 leaf, 1 node
+            (vec!["i", "ii"], 2, 1), //1 leaf, 2 total "nodes"
+            (vec!["ii", "iiiii"], 5, 1), //1 leaf, 5 total "nodes"
+            (vec!["ii", "iii", "iiiii", "iiiiiii"], 7, 1), //1 leaf, 7 total "nodes"
+            (vec!["ii", "iiii", "iij", "iijjj"], 7, 3), //2 leaves, 1 fork, 7 total "nodes"
+        ];
+        for (keys, expected_sum_ordinary, expected_sum_jumping) in tests {
+            let map: BytesTrieMap<()> = keys.into_iter().map(|v| (v, ())).collect();
+            let zip = map.read_zipper();
+
+            let map_f = |_v: &(), _path: &[u8]| {
+                // println!("map path=\"{}\"", String::from_utf8_lossy(_path));
+                1
+            };
+            let collapse_f = |_v: &(), upstream: u32, _path: &[u8]| {
+                // println!("collapse path=\"{}\", upstream={upstream}", String::from_utf8_lossy(_path));
+                upstream
+            };
+            let alg_f = |_child_mask: &[u64; 4], children: &mut [u32], path: &[u8]| {
+                // println!("aggregate path=\"{}\", children={children:?}", String::from_utf8_lossy(path));
+                let sum = children.into_iter().fold(0, |sum, child| sum + *child);
+                if path.len() > 0 {
+                    sum + 1
+                } else {
+                    sum
+                }
+            };
+
+            //Test both the jumping and non-jumping versions
+            let sum = zip.clone().into_cata_side_effect(map_f, collapse_f, alg_f);
+            assert_eq!(sum, expected_sum_ordinary);
+
+            let sum = zip.into_jumping_cata_side_effect(map_f, collapse_f, alg_f);
+            assert_eq!(sum, expected_sum_jumping);
+        }
+    }
+
 }
