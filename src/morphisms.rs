098424194b16162eb29523212c51b9f8c45705b2
Luke Peterson <luke@infoplatter.com>
2025-01-24 18:15:13 +0900

    Simplifying the "Greetings" anamorphism test, and adding a lot of comments

--- src/morphisms.rs
+++ src/morphisms.rs
@@ -559,7 +559,7 @@ impl<W: Default> ChildBuilder<W> {
         self.cur_mask_word = mask_word;
         let mask_delta = 1u64 << (byte % 64);
         if self.child_mask[mask_word] >= mask_delta {
-            panic!("children must be pushed in sorted order")
+            panic!("children must be pushed in sorted order and each initial byte must be unique")
         }
         self.child_mask[mask_word] |= mask_delta;
 
@@ -659,7 +659,6 @@ impl<W: Default> ChildBuilder<W> {
 
 #[cfg(test)]
 mod tests {
-    use core::ptr::slice_from_raw_parts_mut; //GOAT, UNSAFE does NOT belong in these tests.  We don't need every drop of perf in a correctness test!
     use crate::trie_map::BytesTrieMap;
     use super::*;
 
@@ -843,85 +842,9 @@ mod tests {
         assert_eq!(invocations, 15);
     }
 
-    #[test]
-    fn ana_test2() {
-        // to be revisited with futu or reworked ana
-        let file_content = "Hallo,Afrikaans\nPërshëndetje,Albanian\nእው ሰላም ነው,Amharic\nمرحبًا,Arabic\nBarev,Armenian\nKamisaki,Aymara\nSalam,Azerbaijani\nKaixo,Basque\nВітаю,Belarusian\nহ্যালো,Bengali\nZdravo,Bosnian\nЗдравейте,Bulgarian\nဟယ်လို,Burmese\n你好,Cantonese\nHola,Catalan\nKamusta,Cebuano\nMoni,Chichewa\nBonghjornu,Corsican\nZdravo,Croatian\nAhoj,Czech\nHej,Danish\nHallo,Dutch\nHello,English\nTere,Estonian\nHello,Ewe\nسلام,Farsi (Persian)\nBula,Fijian\nKumusta,Filipino\nHei,Finnish\nBonjour,French\nDia dhuit,Gaelic (Irish)\nOla,Galician\nგამარჯობა,Georgian\nGuten tag,German\nγεια,Greek\nMba'éichapa,Guarani\nBonjou,Haitian Creole\nAloha,Hawaiian\nשלום,Hebrew\nनमस्ते,Hindi\nNyob zoo,Hmong\nSzia,Hungarian\nHalló,Icelandic\nNdewo,Igbo\nHello,Ilocano\nHalo,Indonesian\nCiao,Italian\nこんにちは,Japanese\nСәлеметсіз бе,Kazakh\nសួស្តី,Khmer\nMwaramutse,Kinyarwanda\n안녕하세요,Korean\nSlav,Kurdish\nສະບາຍດີ,Lao\nSalve,Latin\nSveika,Latvian\nSveiki,Lithuanian\nMoien,Luxembourgish\nSalama,Malagasy\nSelamat pagi,Malay\nBongu,Maltese\n你好,Mandarin\nKia ora,Maori\nनमस्कार,Marathi\nсайн уу,Mongolian\nNiltze Tialli Pialli,Nahuatl\nYa’at’eeh,Navajo\nनमस्कार,Nepali\nHei,Norwegian\nسلام,Pashto\nCześć,Polish\nOlá,Portuguese\nਸਤ ਸ੍ਰੀ ਅਕਾਲ,Punjabi\nAkkam,Oromo\nAllianchu,Quechua\nBunâ,Romanian\nПривет,Russian\nTalofa,Samoan\nThobela,Sepedi\nЗдраво,Serbian\nDumela,Sesotho\nAhoj,Slovak\nZdravo,Slovenian\nHello,Somali\nHola,Spanish\nJambo,Swahili\nHallå,Swedish\nKamusta,Tagalog\nIa Orana,Tahitian\nLi-hó,Taiwanese\nவணக்கம்,Tamil\nสวัสดี,Thai\nTashi delek,Tibetan\nMālō e lelei,Tongan\nAvuxeni,Tsonga\nMerhaba,Turkish\nпривіт,Ukrainian\nالسلام عليكم,Urdu\nSalom,Uzbek\nXin chào,Vietnamese\nHelo,Welsh\nMolo,Xhosa\n";
-        let mut lines = file_content.lines().collect::<Vec<_>>();
-
-        let _btm = BytesTrieMap::<&str>::new_from_ana(lines.as_mut_slice(), |ss, val, children, _path| {
-            // println!("at path {}", std::str::from_utf8(_path).unwrap_or("err"));
-            if !ss.is_empty() {
-                if ss.len() == 1 {
-                    if ss[0] != "" {
-                        let det = ss[0].find(',').unwrap_or(usize::MAX);
-                        if det == 0 {
-                            // println!("setting_singular {}", &ss[0][1..]);
-                            *val = Some(&ss[0][1..])
-                        } else if det == usize::MAX {
-                            // println!("setting_singular_noprefix {}", &ss[0][1..]);
-                            *val = Some(&ss[0])
-                        } else {
-                            // println!("pushing_singular {}", &ss[0][0..det]);
-                            let k = ss[0][0..det].as_bytes();
-                            ss[0] = &ss[0][det..];
-                            children.push(k, &mut ss[0..1]);
-                        }
-                    }
-                } else {
-                    ss.sort_by_key(|s| s.chars().next());
-                    // println!("chunk {} {}", ss.len(), ss.into_iter().map(|s| s.chars().next().unwrap_or('\'')).collect::<String>());
-                    // println!("chunk {:?}", ss);
-                    let mut i = 0;
-                    let mut j = 1;
-                    while ss[i].is_empty() {
-                        i += 1;
-                        j += 1;
-                    }
-                    let mut ib = ss[i].chars().next().unwrap();
-                    // println!("processing {}", ss[i]);
-                    if ib == ',' {
-                        // println!("setting0 {}", ss[i]);
-                        *val = Some(&ss[i][1..]);
-                        ss[i] = "";
-                    }
-                    for s in unsafe { slice_from_raw_parts_mut(ss[j..].as_ptr().cast_mut(), ss.len() - 1).as_mut().unwrap() }.iter_mut() {
-                        let jb = s.chars().next().unwrap();
-                        // println!("processing {}", s);
-                        if jb == ',' {
-                            // println!("setting {}", &s[1..]);
-                            *val = Some(&s[1..]);
-                            *s = "";
-                            j += 1;
-                            continue;
-                        }
-                        if jb != ib {
-                            let mut buf = [0u8; 4];
-                            let bs = char::encode_utf8(ib, buf.as_mut()).as_bytes();
-                            children.push(bs, unsafe { slice_from_raw_parts_mut(ss[i..j].as_ptr().cast_mut(), j - i).as_mut().unwrap() });
-                            // println!("pushing {i}..{j} @ {}", std::str::from_utf8(bs).unwrap_or("err"));
-                            i = j;
-                            ib = jb;
-                        }
-                        let mut it = s.char_indices(); it.next().unwrap();
-                        // println!("old {}, new {}", s, &s[it.next().unwrap().0..]);
-                        *s = &s[it.next().unwrap().0..];
-                        j += 1;
-                    }
-                    // currently doesn't push last chunk; whatever
-                }
-            }
-        });
-
-        let mut rz = _btm.read_zipper();
-        while let Some(language) = rz.to_next_val() {
-            println!("language: {}, greeting: {}", language, std::str::from_utf8(rz.path()).unwrap());
-        }
-    }
-
     /// Test the [`ChildBuilder::set_child_mask`] API to set multiple children at once
     #[test]
-    fn ana_test3() {
+    fn ana_test2() {
         let map: BytesTrieMap<()> = BytesTrieMap::<()>::new_from_ana(([0u64; 4], 0), |(mut mask, idx), val, children, _path| {
             // println!("path=\"{}\"", String::from_utf8_lossy(_path));
             if idx < 5 {
@@ -939,7 +862,7 @@ mod tests {
     }
     /// Test the [`ChildBuilder::push`] API to set whole string paths
     #[test]
-    fn ana_test4() {
+    fn ana_test3() {
         let map: BytesTrieMap<()> = BytesTrieMap::<()>::new_from_ana(3, |idx, val, children, _path| {
             // println!("path=\"{}\"", String::from_utf8_lossy(_path));
             if idx > 0 {
@@ -999,6 +922,81 @@ mod tests {
         assert_eq!(map.get(b"RightLeftRightLeftRightLeftRight"), Some(&()));
         // for (path, ()) in map.iter() {
         //     println!("{}", String::from_utf8_lossy(&path));
+        // }
+    }
+
+    const GREETINGS: &[&str] = &["Hallo,Afrikaans", "Përshëndetje,Albanian", "እው ሰላም ነው,Amharic", "مرحبًا,Arabic",
+        "Barev,Armenian", "Kamisaki,Aymara", "Salam,Azerbaijani", "Kaixo,Basque", "Вітаю,Belarusian", "হ্যালো,Bengali",
+        "Zdravo,Bosnian", "Здравейте,Bulgarian", "ဟယ်လို,Burmese", "你好,Cantonese", "Hola,Catalan", "Kamusta,Cebuano",
+        "Kamusta,Cebuano", "Moni,Chichewa", "Bonghjornu,Corsican", "Zdravo,Croatian", "Ahoj,Czech", "Hej,Danish",
+        "Hallo,Dutch", "Hello,English", "Tere,Estonian", "Hello,Ewe", "سلام,Farsi (Persian)", "Bula,Fijian",
+        "Kumusta,Filipino", "Hei,Finnish", "Bonjour,French", "Dia dhuit,Gaelic (Irish)", "Ola,Galician", "გამარჯობა,Georgian",
+        "Guten tag,German", "γεια,Greek", "Mba'éichapa,Guarani", "Bonjou,Haitian Creole", "Aloha,Hawaiian",
+        "שלום,Hebrew", "नमस्ते,Hindi", "Nyob zoo,Hmong", "Szia,Hungarian", "Halló,Icelandic", "Ndewo,Igbo",
+        "TRASH-NO-COMMA", //Trash here, to test error-cases
+        "Hello,Ilocano", "Halo,Indonesian", "Ciao,Italian", "こんにちは,Japanese", "Сәлеметсіз бе,Kazakh",
+        "TRASH-NOTHING-AFTER-COMMA,", //Trash here, to test error-cases
+        "សួស្តី,Khmer", "Mwaramutse,Kinyarwanda", "안녕하세요,Korean", "Slav,Kurdish", "ສະບາຍດີ,Lao", "Salve,Latin",
+        ",TRASH-NOTHING-BEFORE-COMMA", //Trash here, to test error-cases
+        "Sveika,Latvian", "Sveiki,Lithuanian", "Moien,Luxembourgish", "Salama,Malagasy", "Selamat pagi,Malay",
+        "", //Trash here, empty string
+        "Bongu,Maltese", "你好,Mandarin", "Kia ora,Maori", "नमस्कार,Marathi", "сайн уу,Mongolian", "Niltze Tialli Pialli,Nahuatl",
+        "Ya’at’eeh,Navajo", "नमस्कार,Nepali", "Hei,Norwegian", "سلام,Pashto", "Cześć,Polish", "Olá,Portuguese",
+        "ਸਤ ਸ੍ਰੀ ਅਕਾਲ,Punjabi", "Akkam,Oromo", "Allianchu,Quechua", "Bunâ,Romanian", "Привет,Russian", "Talofa,Samoan",
+        "Thobela,Sepedi", "Здраво,Serbian", "Dumela,Sesotho", "Ahoj,Slovak", "Zdravo,Slovenian", "Hello,Somali",
+        "Hola,Spanish", "Jambo,Swahili", "Hallå,Swedish", "Kamusta,Tagalog", "Ia Orana,Tahitian", "Li-hó,Taiwanese",
+        "வணக்கம்,Tamil", "สวัสดี,Thai", "Tashi delek,Tibetan", "Mālō e lelei,Tongan", "Avuxeni,Tsonga", "Merhaba,Turkish",
+        "привіт,Ukrainian", "السلام عليكم,Urdu", "Salom,Uzbek", "Xin chào,Vietnamese", "Helo,Welsh", "Molo,Xhosa",
+    ];
+
+    /// Test pushing into the trie, one byte at a time
+    #[test]
+    fn ana_test4() {
+        let mut greetings_vec = GREETINGS.to_vec();
+        let _btm = BytesTrieMap::<&str>::new_from_ana((&mut greetings_vec[..], 0), |(mut string_slice, n), val, children, _path| {
+            // println!("at path {}", std::str::from_utf8(_path).unwrap_or("err"));
+
+            //Sort the slice by the first byte of each substring
+            string_slice.sort_by_key(|s| s.as_bytes().get(n));
+
+            while string_slice.len() > 0 {
+
+                //Discard the strings that are too short (that have ended prematurely)
+                let mut i=0;
+                while i < string_slice.len() && string_slice[i].len() <= n {
+                    i += 1;
+                }
+                (_, string_slice) = string_slice.split_at_mut(i);
+                if string_slice.len() == 0 {
+                    return
+                }
+
+                //Find the range of strings that start with the same byte as the first string
+                let mut i = 1;
+                let byte = string_slice[0].as_bytes()[n];
+                while i < string_slice.len() && string_slice[i].as_bytes()[n] == byte {
+                    i += 1;
+                }
+                let (same_prefix_set, remaining) = string_slice.split_at_mut(i);
+
+                //If this is the end of a path, set the value
+                if byte == b',' {
+
+                    //There may be multiple values for this key, but we can only set one! :-(
+                    let byte_slice = &same_prefix_set[0].as_bytes()[n+1..];
+                    *val = Some(std::str::from_utf8(byte_slice).unwrap_or("err"))
+                } else {
+
+                    //Recursive case
+                    children.push_byte(byte, (same_prefix_set, n+1));
+                }
+                string_slice = remaining;
+            }
+        });
+
+        // let mut rz = _btm.read_zipper();
+        // while let Some(language) = rz.to_next_val() {
+        //     println!("language: {}, greeting: {}", language, std::str::from_utf8(rz.path()).unwrap());
         // }
     }
 }
